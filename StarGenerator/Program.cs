07bc7962fb001d4c2d6837835b4b976ae3bdb4b1
Aaron Olson <presin16@gmail.com>
2019-10-20 15:32:12 -0500

    Updated to allow multiple stars in one run.

--- StarGenerator/Program.cs
+++ StarGenerator/Program.cs
@@ -12,38 +12,37 @@ namespace StarGenerator
         static void Main(string[] args)
         {
             // Random name Generator
-            Console.WriteLine("Welcome to the Star System Generator! To generate a star system, press any key.");
-            Console.ReadLine();
-            string letter;
-            do
+            Console.WriteLine("Welcome to the Star System Generator! How many stars would you like to generate?");
+            int stars = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < stars; i++)
             {
                 NameStar();
-                // GeneratePlanets();
-                Console.WriteLine("Press Y to generate another system, or any other key to exit.");
-                letter = Console.ReadLine();
+                // Console.WriteLine("Press Y to generate another system, or any other key to exit.");
+                GeneratePlanets();
+
             }
-            while (letter == "Y" || letter == "y");
+            Console.ReadLine();
         }
 
         static void NameStar()
         {
-            Random rand = new Random();
+            Random randStarName = new Random();
 
             string[] starSyllables = { "al", "be", "gam", "del", "the", "kap", "io", "chi", "neth", "mat", "than", "bre", "epa", "sil", "on", "cha", "cet", "pha", "ma", "pa", "ta", "nik", "lop", "in", "sia", "ios" };
 
             string starName = null;
-            int numberOfSyllables = rand.Next(2, 5);
+            int numberOfSyllables = randStarName.Next(2, 5);
             for (int i = 0; i < numberOfSyllables; i++)
             {
-                int starSyllableIndex = rand.Next(starSyllables.Length);
+                int starSyllableIndex = randStarName.Next(starSyllables.Length);
                 starName += starSyllables[starSyllableIndex];
                 Thread.Sleep(20);
                 starName = char.ToUpper(starName[0]) + starName.Substring(1);
             }
             char[] starClasses = { 'O', 'B', 'A', 'F', 'G', 'K', 'M' };
-            int starClassIndex = rand.Next(starClasses.Length);
+            int starClassIndex = randStarName.Next(starClasses.Length);
             char starClass = starClasses[starClassIndex];
-            int lightYears = rand.Next(4, 102034);
+            int lightYears = randStarName.Next(4, 102034);
             string lightYearsString = lightYears.ToString("N0");
             Console.WriteLine(starName + " is a class " + starClass + " star, " + lightYearsString + " light years away.");
         }
